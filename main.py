311b5249e47e6c67cd478cc0a15d617d5ed268ae
Tommy Larsen <tommy.larsen@ghakk.se>
2024-02-21 20:51:04 +0100

    Added Lint and linted some files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 
+
 def main():
     if len(sys.argv) < 2:
         toprint = "No printing"
@@ -10,8 +11,11 @@ def main():
         print(hello_print(toprint))
     except Exception as e:
         print(e)
-def hello_print(text_to_print = None):
+
+
+def hello_print(text_to_print=None):
     return (text_to_print)
+
 
 if __name__ == "__main__":
     main()
