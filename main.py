e762a48a64bc717f6c604429a4f61dc5a69ab750
Tommy Larsen <tommy.larsen@ghakk.se>
2024-02-21 20:37:21 +0100

    Commited some stuffs Some stuffs was added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+import sys
 
 def main():
-    toprint = "Hello World"
+    if len(sys.argv) < 2:
+        toprint = "No printing"
+    else:
+        toprint = sys.argv[1]
     try:
         print(hello_print(toprint))
     except Exception as e:
